91e0bd3376d7fc7c80e051cf5a7011e4bae49c02
Saksham <s.s.birla@student.utwente.nl>
2026-06-16 16:19:49 +0200

    env bugs

--- hpc/run_viz_predictions.sh
+++ hpc/run_viz_predictions.sh
@@ -10,6 +10,10 @@
 PROJECT=/home/s3165582/thesis/drone-multimodal-robustness
 cd $PROJECT
 
+# Activate the conda environment that has PyTorch + MMRotate
+source ~/.bashrc
+conda activate thesis
+
 PYTHONPATH=$PROJECT python scripts/visualize_predictions.py \
     --config  experiments/configs/c2former_dronevehicle.py \
     --ckpt    work_dirs/c2former/epoch_24.pth \
