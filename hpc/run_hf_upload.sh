79f5365cdf6e589e5d14cf1e30a43450d4c1d7f1
Saksham <s.s.birla@student.utwente.nl>
2026-06-19 17:36:45 +0200

    new job

---
+++ hpc/run_hf_upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=hf_upload
+#SBATCH --partition=main-cpu
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=2-00:00:00
+#SBATCH --output=logs/hf_upload_%j.log
+
+PYTHON=/home/s3165582/.conda/envs/thesis/bin/python
+
+$PYTHON -c "
+from huggingface_hub import HfApi
+HfApi().upload_large_folder(
+    repo_id='Saksham224/DroneVehicle-C',
+    repo_type='dataset',
+    folder_path='/home/s3165582/thesis/drone-multimodal-robustness/data/DroneVehicle_C',
+    num_workers=4,
+)
+"
